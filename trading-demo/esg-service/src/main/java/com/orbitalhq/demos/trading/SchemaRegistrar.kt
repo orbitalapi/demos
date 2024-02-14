cb40c4b436017a8c998050f4f20ec1c049bc7f55
Serhat Tuncay <serhat@orbitalhq.com>
2024-02-14 12:27:39 +0300

    update to schema publishing

--- trading-demo/esg-service/src/main/java/com/orbitalhq/demos/trading/SchemaRegistrar.kt
+++ trading-demo/esg-service/src/main/java/com/orbitalhq/demos/trading/SchemaRegistrar.kt
@@ -6,24 +6,31 @@ import com.orbitalhq.schema.publisher.SchemaPublisherService
 import com.orbitalhq.schema.publisher.rsocket.RSocketSchemaPublisherTransport
 import com.orbitalhq.schema.rsocket.TcpAddress
 import lang.taxi.generators.java.spring.SpringTaxiGenerator
+import mu.KotlinLogging
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
+private val logger = KotlinLogging.logger {  }
 @Component
 class SchemaRegistrar(@Value("\${server.port}")
-                      private val serverPort: String): InitializingBean {
+                      private val serverPort: String,
+                      @Value("\${orbital.host:localhost}")
+                      private val orbitalHost: String,
+                      @Value("\${service.host:localhost}")
+                      private val serviceHost: String): InitializingBean {
    override fun afterPropertiesSet() {
+      logger.info { "Orbital host => $orbitalHost, service host => $serviceHost" }
       val appName = "trading-demo-service"
       val publisher = SchemaPublisherService(
          appName,
          RSocketSchemaPublisherTransport(
-            TcpAddress("localhost", 7655)
+            TcpAddress(orbitalHost, 7655)
          )
       )
       publisher.publish(
          PackageMetadata.from("com.lunarbank.demo", appName),
-         SpringTaxiGenerator.forBaseUrl("http://localhost:${serverPort}")
+         SpringTaxiGenerator.forBaseUrl("http://$serviceHost:${serverPort}")
             .forPackage(EsgService::class.java)
             .generate()
       ).subscribe()
