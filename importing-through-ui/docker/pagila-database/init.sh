01744bd54313acecb1fd7bd7d65dd20213f1d8f4
Marty Pitt <martypitt@gmail.com>
2024-09-03 13:32:29 +0100

    fix(importing-through-ui): fix db import

--- importing-through-ui/docker/pagila-database/init.sh
+++ importing-through-ui/docker/pagila-database/init.sh
@@ -2,14 +2,15 @@
 set -e
 
 ls -ltra /docker-entrypoint-initdb.d/
-#echo 'Creating Orbital user`'
-psql --username $POSTGRES_USER -d $POSTGRES_DB < /docker-entrypoint-initdb.d/orbital-user.sql
+ls -ltra /docker-entrypoint-initdb.d/dump
+echo 'Creating Orbital user`'
+psql --username $POSTGRES_USER -d $POSTGRES_DB < /docker-entrypoint-initdb.d/dump/orbital-user.sql
 echo 'Importing data into db $POSTGRES_DB'
-psql --username $POSTGRES_USER -d $POSTGRES_DB < /docker-entrypoint-initdb.d/pagila-schema.sql
+psql --username $POSTGRES_USER -d $POSTGRES_DB < /docker-entrypoint-initdb.d/dump/pagila-schema.sql
 echo 'Importing schema finished successfully'
-psql --username $POSTGRES_USER -d $POSTGRES_DB < /docker-entrypoint-initdb.d/pagila-insert-data.sql
+psql --username $POSTGRES_USER -d $POSTGRES_DB < /docker-entrypoint-initdb.d/dump/pagila-insert-data.sql
 echo 'Importing insert-data finished successfully'
-psql --username $POSTGRES_USER -d $POSTGRES_DB < /docker-entrypoint-initdb.d/pagila-data.sql
+psql --username $POSTGRES_USER -d $POSTGRES_DB < /docker-entrypoint-initdb.d/dump/pagila-data.sql
 echo 'Importing data finished successfully'
 
 echo 'Data import finished successfully'
