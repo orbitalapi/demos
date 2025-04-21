7e902e96341c2dc9414f7f4e94e691317fdc38fb
Serhat Tuncay <serhat@orbitalhq.com>
2025-04-21 17:22:52 +0300

    Swagger Definition updates

--- films-api-client-crendentials/films-api-resource-server/src/main/java/com/petflix/oauth/films/resource/server/FilmController.kt
+++ films-api-client-crendentials/films-api-resource-server/src/main/java/com/petflix/oauth/films/resource/server/FilmController.kt
@@ -1,5 +1,7 @@
 package com.petflix.oauth.films.resource.server
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -9,10 +11,14 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/films")
 class FilmController(private val filmsService: FilmsService) {
+    @Operation(summary = "Top 100 IMDB Movies",  description = "Retrieves Top 100 Imdb Movies.",
+        security = [SecurityRequirement(name = "security_auth")])
     @Tag(name = "topImdbMovies", description = "Top 100 Imdb Movies.")
     @GetMapping("/")
     fun getFilms() = filmsService.getFilms()
 
+    @Operation(summary = "A Top 100 Imdb Movie Detail",  description = "Retrieves Details of a Top 100 Imdb Movie.",
+        security = [SecurityRequirement(name = "security_auth")])
     @Tag(name = "findFilm", description = "Find the film for the given Id.")
     @GetMapping("/{id}")
     fun getFilm(@PathVariable id:String) = filmsService.getFilm(id)
---
+++ films-api-client-crendentials/films-api-resource-server/src/main/java/com/petflix/oauth/films/resource/server/OpenAPI3Configuration.kt
@@ -0,0 +1,29 @@
+package com.petflix.oauth.films.resource.server
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+    servers = [Server(url = "https://localhost:8081")], info = Info(
+        title = "Film Service APIs",
+        description = "This lists all Films Service API Calls. The Calls are OAuth2 secured, "
+                + "so please use your client ID and Secret to test them out.",
+        version = "v1.0"
+    )
+)
+@SecurityScheme(
+    name = "security_auth",
+    type = SecuritySchemeType.OAUTH2,
+    flows = OAuthFlows(
+        clientCredentials = OAuthFlow(
+            tokenUrl = "\${petflix.authroisationServer.tokenUrl:http://localhost:9000/oauth2/token}"
+        )
+    )
+)
+class OpenAPI3Configuration
